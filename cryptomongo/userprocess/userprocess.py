c504e08a3c014f23bfd51588154e1f81f03bcf21
Daniel Kumor <daniel@dkumor.com>
2014-11-03 07:46:19 -0600

    Preliminary implementation of final server
    All commands ready, need to be tested and error handled

--- cryptomongo/userprocess/userprocess.py
+++ cryptomongo/userprocess/userprocess.py
@@ -33,46 +33,96 @@ logger = None
 dbhandler = None
 rc = None
 
-class MainHandler(tornado.web.RequestHandler):
+threads = []
 
+def cmdrunner(cmd,handler,cmdfnc,cmdargs):
+
+
+    handler.finish()
+
+class MainHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous   #The requests are funneled to other threads
     def post(self):
         global logger
-        cmd = self.get_argument("cmd")
-        logger.info("CMD: %s"%(cmd,))
+        global dbhandler
 
-        #Handle the accepted commands
-        if (cmd=="connect"):
-            self.send_error(500)
-            """
-            elif (cmd=="create"):
-                self.send_error(500)
-            elif (cmd=="open"):
-                self.send_error(500)
-            elif (cmd=="close"):
-                self.send_error(500)
-            elif (cmd=="panic"):
-                self.send_error(500)
-            elif (cmd=="panicall"):
-                self.send_error(500)
-            elif (cmd=="delete"):
-                self.send_error(500)
-            """
-        #These commands should all be fast - so no need to run them in another thread
-        elif (cmd=="ls"):
+        cmd = self.get_argument("cmd")
+        logger.info("SRC: %s CMD: %s"%(self.request.remote_ip,cmd,))
+        if (cmd=="ls"):
             logger.info("LS command -> listing open dbs")
             self.write(json.dumps(dbhandler.ls()))
+            self.finish()
         elif (cmd=="exists"):
             n = self.get_argument("name")
             logger.info("EXISTS %s",n)
             self.write(json.dumps(dbhandler.exists(n)))
+            self.finish()
         elif (cmd=="isopen"):
             n = self.get_argument("name")
             logger.info("ISOPEN %s",n)
             self.write(json.dumps(dbhandler.isopen(n)))
+            self.finish()
+        else:
+            global threads
 
+            #Remove finished threads - a cleanup operation
+            for t in threads:
+                if not t.isAlive():
+                    # get results from thtead
+                    t.handled = True
+            threads = [t for t in threads if not t.handled]
+
+            #It is one of the asynchronous commands. Run it as such
+            t = threading.Thread(target=self.command)
+            t.start()
+            t.handled = False
+            threads.append(t)
+
+    def command(self):
+        global logger
+        global dbhandler
+
+        cmd = self.get_argument("cmd")
+
+        #Handle the accepted commands
+        if (cmd=="connect"):
+            n = self.get_argument("name")
+            logger.info("CONNECT %s",n)
+            self.write(json.dumps(dbhandler.connect(n)))
+
+        elif (cmd=="create"):
+            n = self.get_argument("name")
+            p = self.get_argument("password")
+            s = self.get_argument("size")
+            logger.info("CREATE %s",n)
+            self.write(json.dumps(dbhandler.create(n,p,s)))
+        elif (cmd=="open"):
+            n = self.get_argument("name")
+            p = self.get_argument("password")
+            logger.info("OPEN %s",n)
+            self.write(json.dumps(dbhandler.open(n,p)))
+        elif (cmd=="close"):
+            n = self.get_argument("name")
+            logger.info("CLOSE %s",n)
+            self.write(json.dumps(dbhandler.close(n)))
+        elif (cmd=="panic"):
+            n = self.get_argument("name")
+            logger.info("PANIC %s",n)
+            self.write(json.dumps(dbhandler.panic(n)))
+        elif (cmd=="panicall"):
+            n = self.get_argument("name")
+            logger.info("CLOSE %s",n)
+            self.write(json.dumps(dbhandler.panicall()))
+        elif (cmd=="delete"):
+            n = self.get_argument("name")
+            logger.info("DELETE %s",n)
+            self.write(json.dumps(dbhandler.delete(n)))
+
+        #These commands should all be fast - so no need to run them in another thread
         else:
             logger.error("Unknown command: \"%s\""%(cmd,))
             self.send_error(500)
+        self.finish()
 
 
 
@@ -80,7 +130,11 @@ def stopserver():
     global dbhandler
     global rc
     global logger
+    global threads
     tornado.ioloop.IOLoop.instance().stop()
+    logger.info("Waiting for threads to join...")
+    for i in threads:
+        i.join()
     logger.info("Closing all containers...")
     dbhandler.closeall()
     rc.shutdown()
